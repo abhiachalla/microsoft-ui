3d2bf46e6df095ee0aa2bd0a2ba11c4ef08cba7d
abhiachalla <70327235+abhiachalla@users.noreply.github.com>
2022-08-15 10:31:34 -0400

    cmoit

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
    export const getProduct = async(productID) => {
-    let PRODUCT_API_BASE_URL = "http://localhost:8080/api/salesforce/products/product/"+productID;
+    let PRODUCT_API_BASE_URL = "https://customer360-api.herokuapp.com/api/salesforce/products/product/"+productID;
         console.log(PRODUCT_API_BASE_URL);
         const result = await axios.get(PRODUCT_API_BASE_URL);
         return result.data;
