b74726a1a3f37f4d90b8be6bc39ad035eb579cf6
abhiachalla <70327235+abhiachalla@users.noreply.github.com>
2022-08-16 02:35:31 -0400

    commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../components/product/Product";
 import '../components/product/Product.scss';
 import abhisalesforce from '../components/product/abhisalesforce.jpeg'
+import user2abhisaslesforce from '../components/product/user2abhisalesforce.jpeg';
+
+const whichUser=(user)=> {
+    if(user===1) {
+        return abhisalesforce;
+    } else {
+        return user2abhisaslesforce;
+    }
+}
 
 const Homepage = () => {
+    const [user,setUser] = useState(1);
+    
+    
+        const HandleClickUser1=()=> {
+        //    useEffect(()=>{
+        //         setUser(i);
+        //     },[])
+        setUser(1);
+        }
+        const HandleClickUser2=()=> {
+
+        //     console.log(i)
+        //     console.log("user is ",user)
+        //    useEffect(()=>{
+        //         setUser(i);
+        //     },[])
+        setUser(2);
+        }
+    
+    
+   
+
     return (
         <>
-        <div>Salesforce</div>
+        <button className="button1" onClick={HandleClickUser1}>User 1</button>
+        <button className="button2" onClick={HandleClickUser2}>User 2</button>
+        <div className="cust360div"> <h1 className="cust360">Customer <span className="span360">360</span></h1></div>
+        <img className="salesImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1EZQG5mRHkybGTwLiVukdrCLDRP471Xq5czk4s3c2vw&s" alt="" />
+        {/* <div className="textC"><h1 className="h1C">Bringing Companies and Customers together!</h1></div> */}
         <ul className='circle-container'>
         <li className='listItem'><Product ID="1" className="product" imageSource="//www.salesforce.com/content/dam/web/en_us/www/images/products/sales-circle.png"  productName = "Sales"/></li>
         <li className='listItem'><Product ID="3" className="product" imageSource="https://www.salesforce.com/content/dam/web/en_us/www/images/products/service-circle.png" productName = "Service"/></li>
@@ -18,7 +53,7 @@ const Homepage = () => {
         <li className='listItem'><Product ID="9" className="product" imageSource="https://www.salesforce.com/content/dam/web/en_us/www/images/products/sustainability-circle.png" productName = "NetZero"/></li>
         <li className='listItem'><Product ID="10" className="product" imageSource="https://www.salesforce.com/content/dam/web/en_us/www/images/products/einstein-ai-circle.png" productName = "Einstein AI"/></li>
         <li className='listItem'><Product ID="11" className="product" imageSource="https://www.salesforce.com/content/dam/web/en_us/www/images/products/platform-circle.png" productName = "Platform"/></li>
-        <li><img className="customer360image" src={abhisalesforce} /></li>
+        <li><img className="customer360image" src={whichUser(user)} /></li>
         
         </ul>
         </>
